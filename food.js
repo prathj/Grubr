f1fbae26c0a9594efecf5605cc2be4fb20bba772
Miqqy <104655313+Miqqy@users.noreply.github.com>
2023-12-04 19:57:09 -0800

    cleaned up the weird 2 files

---
+++ food.js
@@ -0,0 +1,20 @@
+export const DUMMY_DATA = [
+  {
+    dishName: "Fried Rice",
+    type: "Chinese cuisine",
+    photoURL: "https://houseofnasheats.com/wp-content/uploads/2023/01/Chicken-Fried-Rice-Recipe-12-scaled.jpg",
+    id: 1,
+  },
+  {
+    dishName: "Pesto Pasta",
+    type: "Italian cuisine",
+    photoURL: "https://www.cookingclassy.com/wp-content/uploads/2022/02/pesto-pasta-1.jpg",
+    id: 2,
+  },
+  {
+    dishName: "Shrimp Curry",
+    type: "Indonesian cuisine",
+    photoURL: "https://dishingouthealth.com/wp-content/uploads/2022/08/CoconutShrimpSkillet_Styled1.jpg",
+    id: 3,
+  }
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,29 +6,7 @@ import { fetchUserInfoAsync } from 'expo-auth-session'
 import useAuth from '../hooks/useAuth'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Swiper from 'react-native-deck-swiper';
-
-const DUMMY_DATA = [
-  {
-    dishName: "Fried Rice",
-    type: "Chinese cuisine",
-    photoURL: "https://houseofnasheats.com/wp-content/uploads/2023/01/Chicken-Fried-Rice-Recipe-12-scaled.jpg",
-    id: 1,
-  },
-  {
-    dishName: "Pesto Pasta",
-    type: "Italian cuisine",
-    photoURL: "https://www.cookingclassy.com/wp-content/uploads/2022/02/pesto-pasta-1.jpg",
-    id: 2,
-  },
-  {
-    dishName: "Shrimp Curry",
-    type: "Indonesian cuisine",
-    photoURL: "https://dishingouthealth.com/wp-content/uploads/2022/08/CoconutShrimpSkillet_Styled1.jpg",
-    id: 3,
-  }
-
-
-]
+import { DUMMY_DATA } from '../food'
 
 const HomeScreen = () => {
     const navigation = useNavigation();
