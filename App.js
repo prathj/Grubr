2cdda32426c45fe8aa50ab24973f12453e23fc7c
prath_j <craftmasterprath123@gmail.com>
2023-12-05 21:39:17 -0800

    working sign in page

--- App.js
+++ App.js
@@ -9,52 +9,44 @@ import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential } from 'fi
 import { auth } from './firebaseConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoginScreen from './screens/LoginScreen';
+// import { MatchedProvider } from './MatchedContext';
 
-// WebBrowswer.maybeCompleteAuthSession();
+WebBrowswer.maybeCompleteAuthSession();
 
 export default function App() {
 
-  // const [userInfo, setUserInfo] = React.useState();
-  // const [request, response, promptAsync] = Google.useAuthRequest({
-  //   iosClientId: '184146420212-ub2t05hp5edre91vj5fm476uh7lbbval.apps.googleusercontent.com',
-  //   androidClientId:''
-  // });
+  const [userInfo, setUserInfo] = React.useState();
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: '184146420212-ub2t05hp5edre91vj5fm476uh7lbbval.apps.googleusercontent.com',
+    androidClientId:''
+  });
 
-  // React.useEffect(() => {
-  //   if (response?.type == "success") {
-  //     const { id_token } = response.params;
-  //     const credential = GoogleAuthProvider.credential(id_token);
-  //     signInWithCredential(auth, credential);
-  //   }
-  // }, [response]);
+  React.useEffect(() => {
+    if (response?.type == "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
 
-  // React.useEffect(() => {
-  //   const unsub = onAuthStateChanged(auth, async (user) => {
-  //     if (user) {
-  //       console.log(JSON.stringify(user, null, 2));
-  //       setUserInfo(user);
-  //     } else {
-  //       console.log("User is not authenticated")
-  //     }
-  //   });
+  React.useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log(JSON.stringify(user, null, 2));
+        setUserInfo(user);
+      } else {
+        console.log("User is not authenticated")
+      }
+    });
 
-  //   return () => unsub();
-  // }, []);
+    return () => unsub();
+  }, []);
 
-  return (
-    <NavigationContainer>
-      
+  return userInfo ? 
+  ( <NavigationContainer>
       <AuthProvider>
         <StackNavigator />
       </AuthProvider>
-      
-    </NavigationContainer>
-  );
-
-  // return userInfo ? 
-  // (<NavigationContainer>
-  //   <AuthProvider>
-  //     <StackNavigator />
-  //   </AuthProvider>
-  // </NavigationContainer>) : <LoginScreen promptAsync={promptAsync} />;
+    </NavigationContainer>) : <LoginScreen promptAsync={promptAsync} />
+  ;
 };
